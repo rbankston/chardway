5df6c13a3bb1661e6d6fbce19b485ce2c4b25317
Ralph Bankston <ralph.l.bankston@gmail.com>
2014-07-31 02:01:36 -0400

    Added more lines

--- ex1.c
+++ ex1.c
@@ -4,6 +4,9 @@ int main(int argc, char *argv[])
 {
 	puts("Hello World.");
 	puts("This is the beginning of C the hardway");
+	puts("This is another line.");
+	puts("Typing out a 4th line.");
+	puts("Typing out a 5th line.");
 
 	return 0;
 }
